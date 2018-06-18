34ad06a075c9a323cce351be3c3ca87cfa9ae994
MJ <muriloaj@users.noreply.github.com>
2018-06-18 20:47:48 -0300

    just 2 remember

--- src/samples/AmazonSimpleQueueService/SimpleQueueServiceSample.java
+++ src/samples/AmazonSimpleQueueService/SimpleQueueServiceSample.java
@@ -12,6 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 import java.util.List;
 import java.util.Map.Entry;
 
